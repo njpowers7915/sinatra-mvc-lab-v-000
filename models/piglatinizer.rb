1ef5ac7b6ac19e5ef222c16f95fdc5540abaf6d2
Nicholas Powers <njpowers7915@gmail.com>
2018-06-04 02:57:05 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,53 +5,20 @@ class PigLatinizer
     @words = words
   end
   
-  def piglatinize(sent)
-    sent = sent.downcase
-    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    words = sent.split(' ')
-    result = []
+def piglatinize(str)
+  alpha = ('a'..'z').to_a
+  vowels = %w[a e i o u]
+  consonants = alpha - vowels
 
-words.each_with_index do |word, i|
-    translation = ''
-    qu = false
-    if vowels.include? word[0]
-        translation = word + 'ay'
-        result.push(translation)
-    else
-        word = word.split('')
-        count = 0
-        word.each_with_index do |char, index|
-            if vowels.include? char
-                # handle words that start with 'qu'
-                if char == 'u' and translation[-1] == 'q'
-                    qu = true
-                    translation = words[i][count + 1..words[i].length] + translation + 'uay'
-                    result.push(translation)
-                    next
-                end
-                break
-            else
-                # handle words with 'qu' in middle
-                if char == 'q' and word[i+1] == 'u'
-                    qu = true
-                    translation = words[i][count + 2..words[i].length] + 'quay'
-                    result.push(translation)
-                    next
-                else
-                    translation += char
-                end
-                count += 1
-            end
-        end
-        # translation of consonant words without qu
-        if not qu
-            translation = words[i][count..words[i].length] + translation + 'ay'
-            result.push(translation)
-        end
-    end
-
-end
-result.join(' ')
+  if vowels.include?(str[0])
+    str + 'ay'
+  elsif consonants.include?(str[0]) && consonants.include?(str[1])
+    str[2..-1] + str[0..1] + 'ay'
+  elsif consonants.include?(str[0])
+    str[1..-1] + str[0] + 'ay'
+  else
+    str # return unchanged
+  end
 end
   
   
