a5bc73fc01b543c25aac0e62a20d28a5d1fab468
Nicholas Powers <njpowers7915@gmail.com>
2018-06-04 02:51:07 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,10 +10,15 @@ class PigLatinizer
     if array[0] =~ (/[aeiouAEIOU]/)
       shifted = array.join
       final = shifted + "way"
-    elsif (array[0] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/) && array[1] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)) || (array[0] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/) && array[1] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/) && array[2] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/))
+    elsif (array[0] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/) && array[1] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)
       shifted = array.insert(-1, array.delete_at(0))
       again = shifted.insert(-1, array.delete_at(0)).join
       final = again + "ay"
+    elsif (array[0] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/) && array[1] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/) && array[2] =~ (/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)
+      shifted = array.insert(-1, array.delete_at(0))
+      again = shifted.insert(-1, array.delete_at(0))
+      third = again.insert(-1, array.delete_at(0)).join
+      final = third + "ay"
     else
       shifted = array.insert(-1, array.delete_at(0)).join
       final = shifted + "ay"
